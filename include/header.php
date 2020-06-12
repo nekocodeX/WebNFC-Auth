1f5660688fbd738bf5e85866e695c5d618c0fafa
nekocodeX <65624234+nekocodeX@users.noreply.github.com>
2020-06-13 04:53:20 +0900

    Add header.php, footer.php, main.css
    [header.php]
    共通ヘッダーを追加
    [footer.php]
    共通フッターを追加
    [main.css]
    文字コード指定を追加

---
+++ include/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous">
+    <title><?php echo $page_title; ?></title>
+</head>
+
+<body>
