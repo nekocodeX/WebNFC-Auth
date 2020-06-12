e92b41c007b33341705b6261810a8d99096d3572
nekocodeX <65624234+nekocodeX@users.noreply.github.com>
2020-06-13 04:04:25 +0900

    Update config.php
    細かな修正

--- include/config.php
+++ include/config.php
@@ -8,5 +8,5 @@ const G_RECAPTCHA_SITEKEY = "";
 $mysqli = mysqli_connect(DB_HOST, DB_USER, DB_PASSWD, DB_NAME);
 
 if (!$mysqli) {
-    die("[ERROR] DB Connect Error: " . mysqli_connect_error());
+    die("[ERROR] DB connect error: " . mysqli_connect_error());
 }
