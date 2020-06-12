7b0f9660cd74eedf42ea38e38a04461d9c75c00f
nekocodeX <65624234+nekocodeX@users.noreply.github.com>
2020-06-13 04:05:07 +0900

    Update install.php
    DB構築処理の追加

--- install.php
+++ install.php
@@ -1,4 +1,17 @@
 <?php
 include "include/config.php";
 
+// DB構築
+if ($mysqli->query("CREATE TABLE IF NOT EXISTS `users` (
+    `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `user_loginid` varchar(20) NOT NULL,
+    `user_sn` varchar(100) NOT NULL,
+    `user_created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`user_id`)
+)   DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;")) {
+    echo "[INFO] DB initialization is complete, please delete this file!!";
+} else {
+    echo "[ERROR] DB initialization failed";
+}
+
 $mysqli->close();
